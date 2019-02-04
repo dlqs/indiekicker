ea71ead37dca9134c94a6f4aa5ca982781b4ff40
dlqs <donaldleeqs@gmail.com>
2019-02-04 12:54:15 +0800

    remove stric

--- routes/home.js
+++ routes/home.js
@@ -11,8 +11,8 @@ router.get("/", async (req, res, next) => {
                        SELECT t.*, (t.amountfunded / t.amountsought * 100.0) as percentagefunded, \
                        DATE_PART(\'days\', t.duedate - now()) as daysleft \
                        FROM totalfunding t ' +
-                       'WHERE t.amountfunded < t.amountsought AND now() <= t.duedate AND t.amountfunded/t.amountsought > 0.9' +
-                       ' ORDER BY daysleft ASC LIMIT 8'
+                       'WHERE t.amountfunded < t.amountsought AND now() <= t.duedate AND t.amountfunded/t.amountsought > 0' +
+                       ' ORDER BY daysleft ASC LIMIT 4'
     let carouselProjects = await db.query(daysLeftQuery)
 
     // projects ordered by highest amountfunded
